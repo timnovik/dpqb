d77095b65543832d494775bdaa5d7cc19869b671
timkhozzy <60286024+timkhozzy@users.noreply.github.com>
2020-01-26 00:31:22 +0300

    Дижение
    Добавлено движение вражеских NPC, исправлены баги с совместимостью частей кода, внесены мелкие поправки

--- classes.py
+++ classes.py
@@ -1,86 +1,75 @@
-from PIL import Image, ImageTk
-from pygame import *
+import pygame
+pygame.init()
 from functions import *
 from setup import *
+import random as rnd
 
 
 Unit_list = []
 
 
-class AnimDrawer:
-    def __init__(self, animations, window):
-        self.animations = animations
-        self.window = window
-
-    def clear(self):
-        self.animations = []
-
-    def add(self, animation, coords):
-        self.animations.append((animation, coords))
-
-    def draw(self, bg):
-        for i in range(5):
-            for j in range(len(self.animations)):
-                self.animations[j][0].draw(i, self.animations[j][1])
-            display.update()
-            time.delay(200)
-            self.window.blit(bg)
-
-
-class Animation:
-    def __init__(self, paths, window):
-        self.pictures = [image.load(path) for path in paths]
-        self.window = window
-
-    def draw(self, frame, coords):
-        if frame >= len(self.pictures):
-            return 0
-        self.window.blit(self.pictures[frame], coords)
-
-    def clear(self, frame, coords, bg):
-        if frame >= len(self.pictures):
-            return 0
-        self.window.blit(bg, coords)
-
-    def __len__(self):
-        return len(self.pictures)
-
-
 class Object:
-    def __init__(self, pl, size, **anim):
+    def __init__(self, code, pl, size, anim):
+        self.code = code
         self.x = pl[0]
         self.y = pl[1]
-        self.animations = anim
         self.size = size
+        self.anim = anim
         for x in range(self.size[0]):
             for y in range(self.size[1]):
                 Map[self.x + x][self.y + y] = self
 
-    def pl(self, coords=(0, 0)):
+    def pix_pl(self, coords=(0, 0)):
         return (self.x + coords[0]) * TILE_SIZE, (self.y + coords[1]) * TILE_SIZE  # место в пикселях
 
-    def animation(self):
-        # TODO: стоит на месте (анимация)
-        pass
+    def pl(self, coords=(0,0)):
+        return self.x + coords[0], self.y + coords[1]
+
+    def clear(self):
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                Map[self.x + x][self.y + y] = None
+        del self
 
 
 Map = [[None for i in range(MAP_SIZE_Y)] for j in range(MAP_SIZE_X)]
 
 
 class Unit(Object):
-    def __init__(self, speed, hp, attack, defence, pl, size, **anim):
-        Object.__init__(self, pl, size, **anim)
-        self.foe = 0  # количество юнитов, бегущих к тебе
+    def __init__(self, code, speed, hp, attack, cooldown, defence, pl, hitdist, size, anim, die_img):
+        Object.__init__(self, code, pl, size, anim)
+        Unit_list.append(self)
+        self.foe = []  # юниты, бегуще к тебе
         self.max_foe = 2  # максимальное количество юнитов, бегущих к тебе
-        self.ang = 0  # параметр, отвечающий за привлекательность для юнитов, атакующих тебя
+        self.ang = 1  # параметр, отвечающий за привлекательность для юнитов, атакующих тебя
         self.speed = speed
         self.hp = hp
         self.damage = attack
-        self.defence = defence
+        self.defence = defence  # defence
+        self.timer = 0
+        self.COOLDOWN = cooldown
+        self.die_img = die_img
+        self.hitdist = hitdist
 
     def attack(self, other):
-        # TODO: attack
-        pass
+        # self атакует other, если закончилась перезарядка
+        if self.timer <= 0:
+            self.timer = self.COOLDOWN
+            damage = self.damage * other.defence
+            if damage >= other.hp:
+                other.die()
+            else:
+                other.hp -= damage
+
+    def die(self):
+        # стирает все упоминания себя, создаёт на месте себя труп
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                Map[self.x + x][self.y + y] = None
+        Unit_list.pop(Unit_list.find(self))
+        for name in self.foe:
+            name.targ = None
+        Map[self.x][self.y] = Object(-1, self.pl(), self.size, self.die_img)
 
     def rebuild(self, pl):
         for x in range(self.size[0]):
@@ -92,55 +81,63 @@ class Unit(Object):
                 Map[self.x + x][self.y + y] = self
 
 
-
 class Unfriendly(Unit):
-    def __init__(self, speed, hp, attack, defece, pl, size, **anim):
-        Unit.__init__(self, speed, hp, attack, defece, pl, size, **anim)
+    def __init__(self, code, speed, hp, attack, defence, pl, size, anim):
+        Unit.__init__(self, code, speed, hp, attack, defence, pl, size, anim)
         global Unit_list
         Unit_list.append(self)
         self.targ = None
         self.ang = 0
+        self.cnt_mov = 0
 
     def movement(self):
-        # TODO: алгоритм перемещения
-        pass
+        if self.targ is not None:
+            # вероятность потерять цель
+            d = dist(self.pl(), self.targ.pl())
+            mab = (((10 - d // 2) if d > 2 * self.dist else (self.dist - d)) ** 2 - (1 - self.cnt_mov) * 30)
+            if self.cnt_mov == 0:
+                mab = -1
+            if rnd.randrange(100) <= mab:
+                self.losetarg()
+        if self.targ is not None:
+            if dist(self.targ.pl(), self.pl()) <= self.hitdist:
+                self.attack(self.targ)
+            else:
+                movements = bfs(Map, self.pl(), self.targ.pl())[:-1][:self.speed]
+                # все точки, которые прошёл юнтит при перемещении
+                self.rebuild(movements[-1])
+            self.cnt_mov += 1
+        else:
+            x_m = rnd.randint(-self.speed, self.speed)
+            y_m = rnd.randint(-self.speed, self.speed)
+            if Map[self.x + x_m][self.y + y_m] is None:
+                movements = bfs(Map, self.pl(), self.targ.pl())[:-1][:self.speed]
+                # все точки, которые прошёл юнтит при перемещении
+                self.rebuild(movements[-1])
+                self.findtarg()
+            else:
+                self.findtarg()
+                self.movement()
 
     def findtarg(self):
         # поиск цели для перемещения
         self.targ = min(Unit_list,
                         key=lambda other: max(abs(other.x - self.x), abs(other.y - self.y)) * other.ang
-                        if other.foe < other.max_foe
+                        if len(other.foe) < other.max_foe
                         else 0)
-        self.targ.foe += 1
+        self.targ.foe.append(self)
 
     def losetarg(self):
-        self.targ.foe -= 1
+        self.targ.foe.pop(self.targ.foe.find(self))
         self.targ = None
+        self.cnt_mov = 0
 
 
 class Hero(Unit):
-    def __init__(self, speed, hp, attack, defence, pl, size=(2, 2), **anim):
-        Unit.__init__(self, speed, hp, attack, defence, pl, size, **anim)
+    def __init__(self, hp, attack, defence, pl, anim, die_img, size=(2, 2), code=1, speed=1):
+        Unit.__init__(self, code, speed, hp, attack, defence, pl, size, anim, die_img)
+        self.ang = 2
 
-    def move(self, direction, Drawer):
+    def move(self, direction):
         self.rebuild((self.x + direction[0], self.y + direction[1]))
-        if direction[0] >= 0:
-            Drawer.add(self.animations['run_right'], divide_line(self.pl(), self.pl(direction), len(self.animations['run_right'])))
-        else:
-            Drawer.add(self.animations['run_left'], divide_line(self.pl(), self.pl(direction), len(self.animations['run_left'])))
-
-    def key_read(self, Drawer):
-        keys = key.get_pressed()
-        print(keys)
-        direction = (0, 0)
-
-        # обработка всех кнопок, которые нажаты (пока только движение)
-        if keys[MOVE_UP]:
-            direction[1] -= 1
-        if keys[MOVE_DOWN]:
-            direction[1] += 1
-        if keys[MOVE_LEFT]:
-            direction[0] -= 1
-        if keys[MOVE_RIGHT]:
-            direction[0] += 1
-        self.move(direction, Drawer)
+        # TODO: добавить поверку цели перемещения
--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 from pygame import *
 
 
+def dist(a, b):
+    return max(abs(a[0] - b[0]), abs(a[1] - b[1]))
+
+
 def divide_line(coords1, coords2, n):
     dx = coords1[0] - coords2[0]
     dy = coords1[1] - coords2[1]
@@ -20,32 +24,72 @@ def lin2(x, d1, d2):
 
 def arrow(st, fn):
     # переделывает прямую в "змейку" от st до fn
-    x = st[0]
-    y = st[1]
-    res = []
-    x_w = -1 if fn[0] - x < 0 else 1  # полуплоскость по x
-    y_w = -1 if fn[1] - y < 0 else 1  # по y
-    while (x, y) != fn:
-        # двигаемся от % к @
-        print(x, y)
-        dirr = [0, 0]
-        """
-        ###
-        @%@
-        ###
-        """
-        if y - 0.5 <= lin2(x + 0.5, fn, st) <= y + 0.5:
-            dirr[0] += 1
+    x, y = st
+    if x == fn[0]:
+        res = []
+        for i in range(min(y, fn[1]), max(y, fn[1])):
+            res += [(x, i)]
+        return res + [fn]
+    elif y == fn[1]:
+        res = []
+        for i in range(min(x, fn[0]), max(x, fn[0])):
+            res += [(i, y)]
+        return res + [fn]
+    else:
+        x = st[0]
+        y = st[1]
+        res = []
+        x_w = -1 if fn[0] - x < 0 else 1  # полуплоскость по x
+        y_w = -1 if fn[1] - y < 0 else 1  # по y
+        while (x, y) != fn:
+            # двигаемся от % к @
+            print(x, y)
+            dirr = [0, 0]
+            """
+            ###
+            @%@
+            ###
+            """
+            if y - 0.5 <= lin2(x + 0.5, fn, st) <= y + 0.5:
+                dirr[0] += 1
 
-        """
-        #@#
-        #%#
-        #@#
-        """
-        if x - 0.5 <= lin2(y + 0.5, fn[::-1], st[::-1]) <= x + 0.5:
-            dirr[1] += 1
+            """
+            #@#
+            #%#
+            #@#
+            """
+            if x - 0.5 <= lin2(y + 0.5, fn[::-1], st[::-1]) <= x + 0.5:
+                dirr[1] += 1
 
-        res += [(x, y)]
-        x += dirr[0] * x_w
-        y += dirr[1] * y_w
-    return res + [(x, y)]
+            res += [(x, y)]
+            x += dirr[0] * x_w
+            y += dirr[1] * y_w
+        return res + [(x, y)]
+
+
+def bfs(map, coords1, coords2):
+    m = len(map)
+    n = len(map[0])
+    s = {i: [] for i in range(m * n)}
+    used = [0] * n * m
+    sum_used = 0
+    for i in range(1, m):
+        for j in range(1, n):
+            for di in [-1, 0, 1]:
+                for dj in [-1, 0, 1]:
+                    if map[i][j] == map[i + di][j + dj] is None:
+                        s[j + (i - 1) * n - 1].append(j + dj - 1 + (i + di - 1) * n)
+                    if map[i][j] is None and not used[j + (i - 1) * n - 1]:
+                        used[j + (i - 1) * n - 1] = 1
+                        sum_used += 1
+    cur = [[coords1]]
+    while cur:
+        new = []
+        for way in cur:
+            for node in s[way[-1]]:
+                if not used[node]:
+                    new.append(way + [node])
+                    used[node] = 1
+                    if node == coords2[0] + coords2[1] * n:
+                        return new[-1]
+        cur = new
